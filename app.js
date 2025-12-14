855a2808baa35250d1749cdd40410f16da9341c0
BilalShaikh33 <bilalshaikhh339@gmail.com>
2025-12-14 16:50:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+function signUp(){
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    if (email== "" || password== "") {
+        alert("Enter Email or password")
+        
+    }
+    else{
+        const auth = firebase.auth();
+        auth.createUserWithEmailAndPassword(email,password)
+        .then( (userdata) => {
+            console.log(userdata.user.uid)
+        } )
+        .catch( (e) => {
+            console.log(e)
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//try ,catch ,finally=>
+    // try{
+
+    //     console.log("test1")
+    //     console.log("test2")
+    //     console.log(adasd) //crash 
+
+    // }catch(ERROR){
+    //     console.log("Error", ERROR)
+    // }finally{
+    //     console.log("ok")
+
+    // }
